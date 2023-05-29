6bb7d4f2f4cf111a296b4f7dfb3be3c08329fbe8
João Antonio Ferreira <joao.parana@gmail.com>
2023-05-29 16:33:56 -0300

    Some updates

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ if __name__ == "__main__":
     # Cria uma série temporal multivariada com três atributos: timestamp, temperatura e
     # velocidade para o proposito de teste
     ts = TimeSerie(data, format='wide', features_qty=3)
+    print('ts.df.index.name:', "'" + str(ts.df.index.name) + "'")
     cols_str = [name for name in sorted(ts.df.columns)]
     cols_str = ', '.join(cols_str)
     logger.info(f'Dataframe com {len(ts.df.columns)} colunas: {cols_str}')
@@ -127,12 +128,62 @@ if __name__ == "__main__":
     # No futuro tipos str e bool serão suportados também, para os casos envolvendo
     # agregação, classificação e regressão.
 
-    Path('../fpp3-python/data/')
-    # IO.read_csv_file()
+    # Preparando uma Série Temporal Multivariada para teste, com alguns NaNs e
+    # alguns dados inválidos
+    ts_whith_nans: TimeSerie = TimeSerie(data=ts.df, format='wide', features_qty=3)
+    path = Path('ts_01.csv')
+    # Coluna de indice não é considerada no atributo 'iloc'
+    print('ts_whith_nans.df.index.name:', "'" + str(ts_whith_nans.df.index.name) + "'")
+    if ts_whith_nans.df.index.name == 'None':
+        ts_whith_nans.df.iloc[0, 2] = 'Missing'
+        ts_whith_nans.df.iloc[1, 1] = np.nan
+        ts_whith_nans.df.iloc[2, 2] = np.nan
+        ts_whith_nans.df.iloc[3, 2] = 'Bad'
+    else:
+        ts_whith_nans.df.iloc[0, 1] = 'Missing'
+        ts_whith_nans.df.iloc[1, 0] = np.nan
+        ts_whith_nans.df.iloc[2, 1] = np.nan
+        ts_whith_nans.df.iloc[3, 1] = 'Bad'
+    # Gero um CSV com alguns NaNs para teste de leitura
+    IO.dataframe_to_csv_file(ts_whith_nans.df, path)
 
     # --------------------------------------------------------------------------------
 
+    # Lê o arquivo CSV com NaNs e gera um Dataframe sem alterar os tipos de dados
+    path = Path('ts_01.csv')
+    column_types: list[type] = []
+    df_whith_nans = IO.read_csv_file(path, column_types)
+    print(df_whith_nans)
+    print('Tipos das colunas no Dataframe da Série Temporal:')
+    for col in df_whith_nans.columns:
+        for value in df_whith_nans[col]:
+            print(col, '\t', value, type(value))
+
     # --------------------------------------------------------------------------------
+
+    # Verifica se a conversão para tipo Number das colunas é trivial ou se é necessário
+    # fazer mapeamento de valores não numéricos, tais como, 'Bad', 'Missing', etc.
+    numeric_columns: list[str] = list(df_whith_nans.columns[1:])
+    all_problems = IO.check_convertion_to_float(numeric_columns, df_whith_nans)
+    print('all_problems:', all_problems)
+
+    # --------------------------------------------------------------------------------
+
+    # Lê o arquivo CSV e gera um Dataframe com os tipos de dados corretos informados
+    # na lista column_types que deve corresponder na mesma ordem com as colunas.
+    path = Path('ts_01.csv')
+    # TODO: Ver como resolver a conversão para pd.Timestamp, pois o Pandas não está fazendo isso automaticamente
+    column_types: list[type] = [np.float32, np.float32]
+    df_whith_nans = IO.read_csv_file(path, column_types)
+    print('df_whith_nans:\n', df_whith_nans)
+    print('Tipos das colunas no Dataframe da Série Temporal:')
+    for col in df_whith_nans.columns:
+        for value in df_whith_nans[col]:
+            print(col, '\t', value, type(value))
+
+    # --------------------------------------------------------------------------------
+
+
 
     # --------------------------------------------------------------------------------
 
--- src/t8s/io.py
+++ src/t8s/io.py
@@ -2,10 +2,13 @@
 
 from pathlib import Path
 from datetime import datetime
+import csv
+import os
 import yaml
 from abc import ABC, abstractmethod
 from typing import Any, Optional, TypeVar
 
+import numpy as np
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
@@ -16,6 +19,7 @@ logger = LogConfig().getLogger()
 
 import re
 from enum import Enum
+from t8s import get_numeric_regex
 
 null_path: Path = Path('/NullPathObject')
 
@@ -81,3 +85,133 @@ class IO:
         result = True
 
         return result
+
+    # Quando estamos lendo de arquivos CSV podem aparecer valores que não tem uma
+    # representação valida para NaN e portanto atrapalha o processo de inferência
+    # de tipo do Pandas. Assim tokens como  'Configure', 'Bad' e 'NotNamber',
+    # por exemplo, devem ser substituidos por NaN (np.na do numpy) para que possamos
+    # fazer a conversão para float. O método check_convertion_to_float() faz isso.
+    @staticmethod
+    def __check_convertion_to_float(col_name:str, cols: pd.Series) -> tuple[str, list[str]]:
+        assert isinstance(cols, pd.Series)
+        not_float_values = []
+        # Definindo a expressão regular para identificar números de ponto flutuante
+        # padrao = r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?'
+        padrao = get_numeric_regex()
+        # Use o método items() para iterar sobre os itens da série
+        for idx, valor in cols.items():
+            print(idx, valor)
+            if (not re.match(padrao, str(valor))) and (not pd.isna(valor)):
+                if isinstance(valor, str):
+                    if valor not in ['NaN', '-NaN', '-nan', 'nan', '<NA>', 'N/A',
+                                     'NA', 'NULL', 'None', 'n/a', 'null']:
+                        # print(idx, '\t', valor, 'não é uma representação valida para NaN')
+                        not_float_values.append(valor)
+                else:
+                    # print(idx, '\t', valor, 'não e um número valido e nem uma representação valida para NaN')
+                    not_float_values.append(valor)
+        if len(not_float_values) > 0:
+            logger.debug('not_float_values: ' + str(not_float_values) + ' for column: ' + str(col_name))
+
+        return (col_name, not_float_values)
+
+    @staticmethod
+    def check_convertion_to_float(numeric_columns: list[str], df: pd.DataFrame) -> dict[str, list[str]]:
+        all_problems = {}
+        for col in numeric_columns:
+            print('Verificando a conversão para tipo Number da coluna', col)
+            col_name, not_float_values = IO.__check_convertion_to_float(col, df[col])
+            if len(not_float_values) > 0:
+                all_problems.update({col_name: not_float_values})
+        return all_problems
+
+    # path é um Path identificando um nome de arquivo CSV e a extensão deve
+    # ser obrigatoriamente '.csv' (em minusculo). Isto é uma convenção.
+    # Além disso o arquivo deve existir e possuir na primira linha a lista
+    # de nomes das colunas / campos (schema). A quintidade de elementos na
+    # lista 'column_types' deve ser igual a quntidade de colunas no arquivo,
+    # ou seja, deve ser igual ao tamanho da lista de nomes especificada na
+    # primeira coluna do arquivo CSV.
+    @staticmethod
+    def read_csv_file(path: Path, column_types: list[type],
+            nan_values:list[str] = [], parse_dates: list[int] = [1],
+            date_format:str = 'AAAA-MM-DD HH:MM:SS') -> pd.DataFrame:
+        for value in column_types:
+            assert isinstance(value, type), f'Erro: {value} não é um tipo válido'
+
+        # Define o conversor para cada tipo de coluna
+        # Crio um dicionário com os conversores e tipos de colunas
+        converters_tmp = {
+            f'{i}': column_types[i] for i in range(len(column_types))
+        }
+
+        # print('converters:', converters_tmp, type(converters_tmp))
+        result = pd.read_csv(filepath_or_buffer = path, sep=',', delimiter=None,
+            header='infer', dtype=None, engine='c', converters=converters_tmp,
+            na_values=nan_values, keep_default_na=True, na_filter=True, verbose=True,
+            skip_blank_lines=True, parse_dates=parse_dates, date_format=date_format,
+            dayfirst=False, cache_dates=False, compression='infer', thousands=None,
+            decimal='.', quotechar='"', # Not Supported: lineterminator=os.linesep
+            quoting=csv.QUOTE_MINIMAL, doublequote=True, comment=None, encoding='utf-8',
+            encoding_errors='strict', on_bad_lines='error', delim_whitespace=False,
+            low_memory=True, memory_map=False, dtype_backend='numpy_nullable')
+        converters =  {}
+        if len(converters_tmp) > 0:
+            # TODO: Verificar como converter a primeira coluna que é Timestamp e é index.
+            for idx, col in enumerate(result.columns[1:]):
+                converters[col] = converters_tmp[str(idx)]
+            print('converters:', converters, type(converters))
+
+        numeric_columns = list(result.columns[1:])
+        all_problems_on_data = IO.check_convertion_to_float(numeric_columns, result)
+        for key, value in converters.items():
+            assert isinstance(value, type), f'Erro: {value} não é um tipo válido para coluna {key}'
+            # Mapeia os tokens inválidos para NaN
+            if (key in all_problems_on_data.keys()):
+                result[key] = result[key].replace(to_replace=all_problems_on_data[key], value=np.nan)
+            print('coluna:', key, '\ttipo:', value)
+            if '<NA>' in result[key].values:
+                result[key] = result[key].replace(to_replace='<NA>', value='NaN')
+            # Faz a conversão de tipo para cada coluna
+            result[key] = result[key].astype(value)
+            print('coluna:', key, '\ttipo:', value)
+
+        return result
+
+    @staticmethod
+    def dataframe_to_csv_file(df: pd.DataFrame, path: Path) -> None:
+        # Se ocorrer erro devemos lançar Exception
+        #
+        # O Pandas salva por padrão datetime como neste exemplo: "2022-11-29 10:40:55".
+        # Este é um formato de data e hora padrão ISO 8601 que é um padrão internacional
+        # para representação de datas e horas, e é amplamente utilizado em sistemas de
+        # computação e comunicação. O formato ISO 8601 para datas e horas é definido
+        # como "AAAA-MM-DDTHH:MM:SS", onde "AAAA" representa o ano com quatro dígitos,
+        # "MM" representa o mês com dois dígitos, "DD" representa o dia com dois dígitos,
+        # "T" é o separador entre a data e a hora, "HH" representa a hora com dois dígitos,
+        # "MM" representa os minutos com dois dígitos e "SS" representa os segundos com
+        # dois dígitos. No formato "2022-11-29 10:40:55", a data e a hora são separadas
+        # por um espaço em vez do caractere "T", mas ainda segue o padrão ISO 8601
+        # para representação de datas e horas.
+        # Veja: https://www.w3.org/TR/NOTE-datetime
+        df.to_csv(path, sep=',', na_rep='NaN', float_format=None,
+            header=True, index=True, index_label=None, mode='w',
+            encoding='utf-8', compression=None, quoting=csv.QUOTE_MINIMAL,
+            quotechar='"', lineterminator=os.linesep, chunksize=None,
+            date_format=None, doublequote=True, escapechar=None,
+            decimal='.', errors='strict', storage_options=None)
+
+
+if __name__ == "__main__":
+    # Lê o arquivo CSV e gera um Dataframe com os tipos de dados corretos informados
+    # na lista column_types que deve corresponder na mesma ordem com as colunas.
+    path = Path('ts_01.csv')
+    # TODO: Ver como resolver a conversão para pd.Timestamp, pois o Pandas não está fazendo isso automaticamente
+    column_types: list[type] = [np.float32, np.float32]
+    df_whith_nans = IO.read_csv_file(path, column_types)
+    print('df_whith_nans:\n', df_whith_nans)
+    print('Tipos das colunas no Dataframe da Série Temporal:')
+    for col in df_whith_nans.columns:
+        for value in df_whith_nans[col]:
+            print(col, '\t', value, type(value))
+        print('-----------------------------------------------')
