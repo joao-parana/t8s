6c65052bf2814b2a168f7b0bf650f3c0a95b2860
João Antonio Ferreira <joao.parana@gmail.com>
2023-09-18 14:34:54 -0300

    Using PROJECT_ROOT from constants.py definitions

--- src/t8s/log_config.py
+++ src/t8s/log_config.py
@@ -2,6 +2,8 @@ import logging
 from logging import Logger
 from typing import Any
 
+from t8s.constants import PROJECT_ROOT
+
 
 class LogConfigMeta(type):
     """
@@ -27,7 +29,7 @@ class LogConfig(metaclass=LogConfigMeta):
         self.logger = None
         self.initialize_logger(level=logging.INFO)
 
-    def initialize_logger(self, level=logging.INFO, log_file: str = 'logs/timeseries.log'):
+    def initialize_logger(self, level=logging.INFO, log_file: str = f'{PROJECT_ROOT}/logs/timeseries.log'):
         my_global_logger = logging.getLogger(__name__)
         my_global_logger.setLevel(level)
         # logger_handler = RotatingFileHandler('timeseries.log', maxBytes=1_000_000, backupCount=10)
